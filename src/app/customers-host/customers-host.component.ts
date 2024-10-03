54524cd09f57b610bc1657593eb2317f61b144f6
Anatoliy Sysoienko <pistalierro@gmail.com>
2024-10-03 21:30:52 +0300

    init firebase, создал файловую структуру проекта

---
+++ src/app/customers-host/customers-host.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'crud-customers-host',
+  standalone: true,
+  imports: [],
+  templateUrl: './customers-host.component.html',
+  styleUrl: './customers-host.component.scss'
+})
+export class CustomersHostComponent {
+
+}
