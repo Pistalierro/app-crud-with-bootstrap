d0db57153391d4d92839e8a78b9f0dba3d2bd146
Anatoliy Sysoienko <pistalierro@gmail.com>
2024-10-04 11:27:28 +0300

    реализовал получение данных с сервера методом GET, заполнил таблицу с покупателями

--- src/app/customers-list/customers-list.component.ts
+++ src/app/customers-list/customers-list.component.ts
@@ -1,12 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpService} from '../shared/services/http.service';
+import {Observable} from 'rxjs';
+import {CustomerInterface} from '../shared/types/customer.interface';
+import {AsyncPipe, NgForOf} from '@angular/common';
 
 @Component({
   selector: 'crud-customers-list',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    AsyncPipe
+  ],
   templateUrl: './customers-list.component.html',
   styleUrl: './customers-list.component.scss'
 })
-export class CustomersListComponent {
+export class CustomersListComponent implements OnInit {
 
+  customers$!: Observable<CustomerInterface[]>;
+
+  constructor(private httpService: HttpService) {
+  }
+
+  ngOnInit(): void {
+    this.customers$ = this.httpService.getData();
+  }
 }
--- src/app/shared/services/http.service.ts
+++ src/app/shared/services/http.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {CustomerInterface} from '../types/customer.interface';
 import {catchError, map, Observable, of} from 'rxjs';
 import {RequestCustomerInterface} from '../types/requestCustomer.interface';
+import {ResponseCustomerInterface} from '../types/responseCustomer.interface';
 
 const url = 'https://crud-with-bootstrap-21290-default-rtdb.europe-west1.firebasedatabase.app/customers';
 const httpOptions = {
@@ -14,19 +15,26 @@ const httpOptions = {
 })
 export class HttpService {
 
-  customers!: CustomerInterface[];
+  customers: CustomerInterface[] = [];
 
   constructor(private http: HttpClient) {
   }
 
   createData(customer: CustomerInterface): Observable<CustomerInterface> {
     return this.http.post<RequestCustomerInterface>(`${url}.json`, customer, httpOptions).pipe(
-      map((res: RequestCustomerInterface) => ({...{key: res.name, ...customer}})),
+      map((res: RequestCustomerInterface) => this.customers.push({...{key: res.name, ...customer}})),
       catchError(this.errorHandler<RequestCustomerInterface>('POST'))
     );
   }
 
-  getData() {
+  getData(): Observable<CustomerInterface[]> {
+    return this.http.get<ResponseCustomerInterface>(`${url}.json`).pipe(
+      map(res => {
+        Object.keys(res).forEach(key => this.customers.push({key, ...res[key]}));
+        return this.customers;
+      }),
+      catchError(this.errorHandler<ResponseCustomerInterface>('GET'))
+    );
   }
 
   updateData() {
